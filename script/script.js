de1e2a24eb6713e34fb12ac7fdcf559bbb6f13ac
shubhshrma <shubham.31996@gmail.com>
2017-08-02 23:47:59 +0530

    Update script

--- script/script.js
+++ script/script.js
@@ -28,72 +28,83 @@ $(document).ready(function(){
 	    }
 	}
 
-	function checkGame(){
+	function checkCompGame(){
+            return checkGame(compSymb,userSymb);
+	}
+	function checkUserGame(){
+			return checkGame(userSymb,compSymb);
+	}
+
+	function checkGame(symb1,symb2){
 		
-		if( (arr[0]==userSymb && arr[1]==userSymb && arr[2]!==compSymb) )
+		if( (arr[0]==symb1 && arr[1]==symb1 && arr[2]!==symb2) )
 			return 2;
-		if((arr[1]==userSymb && arr[2]==userSymb && arr[0]!==compSymb))
+		if((arr[1]==symb1 && arr[2]==symb1 && arr[0]!==symb2))
 			return 0;
-		if((arr[2]==userSymb && arr[0]==userSymb && arr[1]!==compSymb) )
+		if((arr[2]==symb1 && arr[0]==symb1 && arr[1]!==symb2) )
 			return 1;
 
-		if( (arr[3]==userSymb && arr[4]==userSymb && arr[5]!==compSymb) )
+		if( (arr[3]==symb1 && arr[4]==symb1 && arr[5]!==symb2) )
 			return 5;
-		if((arr[4]==userSymb && arr[5]==userSymb && arr[3]!==compSymb))
+		if((arr[4]==symb1 && arr[5]==symb1 && arr[3]!==symb2))
 			return 3;
-		if((arr[3]==userSymb && arr[5]==userSymb && arr[4]!==compSymb) )
+		if((arr[3]==symb1 && arr[5]==symb1 && arr[4]!==symb2) )
 			return 4;
 
-		if( (arr[6]==userSymb && arr[7]==userSymb && arr[8]!==compSymb) )
+		if( (arr[6]==symb1 && arr[7]==symb1 && arr[8]!==symb2) )
 			return 8;
-		if((arr[7]==userSymb && arr[8]==userSymb && arr[6]!==compSymb))
+		if((arr[7]==symb1 && arr[8]==symb1 && arr[6]!==symb2))
 			return 6;
-		if((arr[6]==userSymb && arr[8]==userSymb  && arr[7]!==compSymb) )
+		if((arr[6]==symb1 && arr[8]==symb1  && arr[7]!==symb2) )
 			return 7;
 
-		if( (arr[0]==userSymb && arr[3]==userSymb && arr[6]!==compSymb) )
+		if( (arr[0]==symb1 && arr[3]==symb1 && arr[6]!==symb2) )
 			return 6;
-		if((arr[3]==userSymb && arr[6]==userSymb && arr[0]!==compSymb))
+		if((arr[3]==symb1 && arr[6]==symb1 && arr[0]!==symb2))
 			return 0;
-		if((arr[0]==userSymb && arr[6]==userSymb && arr[3]!==compSymb) )
+		if((arr[0]==symb1 && arr[6]==symb1 && arr[3]!==symb2) )
 			return 3;
 
-		if( (arr[1]==userSymb && arr[4]==userSymb && arr[7]!==compSymb) )
+		if( (arr[1]==symb1 && arr[4]==symb1 && arr[7]!==symb2) )
 			return 7;
-		if((arr[4]==userSymb && arr[7]==userSymb && arr[1]!==compSymb))
+		if((arr[4]==symb1 && arr[7]==symb1 && arr[1]!==symb2))
 			return 1;
-		if((arr[1]==userSymb && arr[7]==userSymb && arr[4]!==compSymb) )
+		if((arr[1]==symb1 && arr[7]==symb1 && arr[4]!==symb2) )
 			return 4;
 
-		if( (arr[2]==userSymb && arr[5]==userSymb && arr[8]!==compSymb) )
+		if( (arr[2]==symb1 && arr[5]==symb1 && arr[8]!==symb2) )
 			return 8;
-		if((arr[5]==userSymb && arr[8]==userSymb && arr[2]!==compSymb))
+		if((arr[5]==symb1 && arr[8]==symb1 && arr[2]!==symb2))
 			return 2;
-		if((arr[2]==userSymb && arr[8]==userSymb && arr[5]!==compSymb) )
+		if((arr[2]==symb1 && arr[8]==symb1 && arr[5]!==symb2) )
 			return 5;
 
-		if( (arr[0]==userSymb && arr[4]==userSymb && arr[8]!==compSymb) )
+		if( (arr[0]==symb1 && arr[4]==symb1 && arr[8]!==symb2) )
 			return 8;
-		if((arr[4]==userSymb && arr[8]==userSymb && arr[0]!==compSymb))
+		if((arr[4]==symb1 && arr[8]==symb1 && arr[0]!==symb2))
 			return 0;
-		if((arr[0]==userSymb && arr[8]==userSymb && arr[4]!==compSymb) )
+		if((arr[0]==symb1 && arr[8]==symb1 && arr[4]!==symb2) )
 			return 4;
 
-		if( (arr[2]==userSymb && arr[4]==userSymb && arr[6]!==compSymb) )
+		if( (arr[2]==symb1 && arr[4]==symb1 && arr[6]!==symb2) )
 			return 6;
-		if((arr[4]==userSymb && arr[6]==userSymb && arr[2]!==compSymb))
+		if((arr[4]==symb1 && arr[6]==symb1 && arr[2]!==symb2))
 			return 2;
-		if((arr[2]==userSymb && arr[6]==userSymb && arr[4]!==compSymb) )
+		if((arr[2]==symb1 && arr[6]==symb1 && arr[4]!==symb2) )
 			return 4;
+
 
         return -1;
 
     }
 
 	function compTurn(){
-		var ch=checkGame();
-		if(ch!==-1)
-			setCompSymb(ch);
+		var u=checkUserGame();
+		var c=checkCompGame();
+		if(c>-1)
+			setCompSymb(c);
+		else if(u!==-1)
+			setCompSymb(u);
 			
 		else
 		{
@@ -174,6 +185,38 @@ $(document).ready(function(){
           				setCompSymb(8);
           		}
             	
+            }
+            else if(k >= 3)
+            {
+            	
+            	/*while(f===0){
+            		var temp=Math.round(Math.random()*8);
+            		if(arr[temp] !== userSymb && arr[temp] !== compSymb){
+            			f=1;
+            			setCompSymb(temp);
+
+            		}
+            	}*/
+            	var f=Math.round(Math.random());
+            	if(f===1){
+            		for(var i=8;i>=0;i--){
+            			if(arr[i]!==compSymb && arr[i]!==userSymb)
+            			{
+            				setCompSymb(i);
+            				break;
+            			}
+            		}
+            	}
+            	else{
+            		for(var i=0;i<=8;i++){
+            			if(arr[i]!==compSymb && arr[i]!==userSymb)
+            			{
+            				setCompSymb(i);
+            				break;
+            			}
+            		}
+
+            	}
             }
 		}
 	}
